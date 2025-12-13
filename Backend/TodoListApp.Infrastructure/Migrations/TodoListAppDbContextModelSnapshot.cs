eb94283c8a73504c1264441871d48c762bb06805
Serhii Moskalets <s.mosklaets16@gmail.com>
2025-12-13 17:23:59 +0100

    .

--- Backend/TodoListApp.Infrastructure/Migrations/TodoListAppDbContextModelSnapshot.cs
+++ Backend/TodoListApp.Infrastructure/Migrations/TodoListAppDbContextModelSnapshot.cs
@@ -6,6 +6,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using TodoListApp.Infrastructure.Persistence;
 
+
 #nullable disable
 
 namespace TodoListApp.Infrastructure.Migrations
