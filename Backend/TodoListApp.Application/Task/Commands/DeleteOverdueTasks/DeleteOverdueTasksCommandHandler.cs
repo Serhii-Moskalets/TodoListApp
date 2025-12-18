75ad73f480d37ca551d19bb02ba840b9889b7ea6
Serhii Moskalets <s.mosklaets16@gmail.com>
2025-12-18 21:36:58 +0100

    feat(application): add DeleteOverdueTasksCommandHandler

---
+++ Backend/TodoListApp.Application/Task/Commands/DeleteOverdueTasks/DeleteOverdueTasksCommandHandler.cs
@@ -0,0 +1,36 @@
+﻿using TinyResult;
+using TodoListApp.Application.Abstractions.Messaging;
+using TodoListApp.Domain.Interfaces.UnitOfWork;
+
+namespace TodoListApp.Application.Task.Commands.DeleteOverdueTasks;
+
+/// <summary>
+/// Handles the <see cref="DeleteOverdueTasksCommand"/> and deletes all overdue tasks.
+/// in a specified task list for a given user.
+/// </summary>
+public class DeleteOverdueTasksCommandHandler : ICommandHandler<DeleteOverdueTasksCommand>
+{
+    private readonly IUnitOfWork _unitOfWork;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="DeleteOverdueTasksCommandHandler"/> class.
+    /// </summary>
+    /// <param name="unitOfWork">The unit of work used to manage repositories and save changes.</param>
+    public DeleteOverdueTasksCommandHandler(IUnitOfWork unitOfWork)
+    {
+        this._unitOfWork = unitOfWork;
+    }
+
+    /// <summary>
+    /// Handles the deletion of overdue tasks in the specified task list for a given user.
+    /// </summary>
+    /// <param name="command">The <see cref="DeleteOverdueTasksCommand"/> containing task list and user identifiers.</param>
+    /// <param name="cancellationToken">A <see cref="CancellationToken"/> to observe while waiting for the operation to complete.</param>
+    /// <returns>A <see cref="Result{Boolean}"/> indicating success or failure of the operation.</returns>
+    public async Task<Result<bool>> Handle(DeleteOverdueTasksCommand command, CancellationToken cancellationToken)
+    {
+        await this._unitOfWork.Tasks.DeleteOverdueTasksAsync(command.UserId, command.TaskListId, DateTime.UtcNow, cancellationToken);
+        await this._unitOfWork.SaveChangesAsync(cancellationToken);
+        return await Result<bool>.SuccessAsync(true);
+    }
+}
