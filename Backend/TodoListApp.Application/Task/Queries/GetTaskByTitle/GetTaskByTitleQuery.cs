e2dc96805f7b2c8cabcb5016b93380f023c79cbc
Serhii Moskalets <s.mosklaets16@gmail.com>
2025-12-20 15:44:50 +0100

    feat(application): add GetTaskByTitleQuery

---
+++ Backend/TodoListApp.Application/Task/Queries/GetTaskByTitle/GetTaskByTitleQuery.cs
@@ -0,0 +1,12 @@
+﻿using TodoListApp.Application.Abstractions.Messaging;
+using TodoListApp.Application.Task.Dtos;
+
+namespace TodoListApp.Application.Task.Queries.GetTaskByTitle;
+
+/// <summary>
+/// Represents a query to retrieve a task by its title (or partial title) for a specific user.
+/// </summary>
+/// <param name="UserId">The identifier of the user who owns the task.</param>
+/// <param name="Text">The title or text to search for in the task's title.</param>
+public sealed record GetTaskByTitleQuery(Guid UserId, string Text)
+    : IQuery<TaskDto>;
