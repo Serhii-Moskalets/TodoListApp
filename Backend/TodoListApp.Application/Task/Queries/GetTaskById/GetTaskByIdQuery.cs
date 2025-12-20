df78f7337876e5fa583288a583b7dd69a22c7657
Serhii Moskalets <s.mosklaets16@gmail.com>
2025-12-20 14:59:50 +0100

    feat(application): add GetTaskByIdQuery

---
+++ Backend/TodoListApp.Application/Task/Queries/GetTaskById/GetTaskByIdQuery.cs
@@ -0,0 +1,12 @@
+﻿using TodoListApp.Application.Abstractions.Messaging;
+using TodoListApp.Application.Task.Dtos;
+
+namespace TodoListApp.Application.Task.Queries.GetTaskById;
+
+/// <summary>
+/// Represents a query to retrieve a specific task by its identifier for a given user.
+/// </summary>
+/// <param name="UserId">The identifier of the user who owns the task.</param>
+/// <param name="TaskId">The identifier of the task to retrieve.</param>
+public sealed record GetTaskByIdQuery(Guid UserId, Guid TaskId)
+    : IQuery<TaskDto>;
