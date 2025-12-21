57ca67f3586c003e1081551b5a6599815c0b5c2a
Serhii Moskalets <s.mosklaets16@gmail.com>
2025-12-21 20:49:13 +0100

    feat(application): install FluentValidation and add ValidateAsync for command/query validation in HandlerBase

--- Backend/TodoListApp.Application/Abstractions/Messaging/HandlerBase.cs
+++ Backend/TodoListApp.Application/Abstractions/Messaging/HandlerBase.cs
@@ -1,4 +1,6 @@
-﻿using TodoListApp.Domain.Interfaces.UnitOfWork;
+﻿using FluentValidation;
+using TinyResult;
+using TodoListApp.Domain.Interfaces.UnitOfWork;
 
 namespace TodoListApp.Application.Abstractions.Messaging;
 
@@ -7,19 +9,45 @@ namespace TodoListApp.Application.Abstractions.Messaging;
 /// Provides shared logic and services that all handlers may need,
 /// such as access to <see cref="IUnitOfWork"/> for repository operations.
 /// </summary>
-public abstract class HandlerBase
+/// <remarks>
+/// Initializes a new instance of the <see cref="HandlerBase"/> class with the specified unit of work.
+/// </remarks>
+/// <param name="unitOfWork">An <see cref="IUnitOfWork"/> instance for working with repositories.</param>
+public abstract class HandlerBase(IUnitOfWork unitOfWork)
 {
-    /// <summary>
-    /// Initializes a new instance of the <see cref="HandlerBase"/> class with the specified unit of work.
-    /// </summary>
-    /// <param name="unitOfWork">An <see cref="IUnitOfWork"/> instance for working with repositories.</param>
-    protected HandlerBase(IUnitOfWork unitOfWork)
-    {
-        this.UnitOfWork = unitOfWork;
-    }
-
     /// <summary>
     /// Gets the unit of work for accessing repositories.
     /// </summary>
-    protected IUnitOfWork UnitOfWork { get; }
+    protected IUnitOfWork UnitOfWork { get; } = unitOfWork;
+
+    /// <summary>
+    /// Performs validation of the given command or query using FluentValidation.
+    /// </summary>
+    /// <typeparam name="TCommand">The type of the command or query to validate.</typeparam>
+    /// <param name="validator">
+    /// The validator instance for the specific command or query type.
+    /// If <c>null</c> is provided, validation is skipped.
+    /// </param>
+    /// <param name="command">The command or query instance to validate.</param>
+    /// <returns>
+    /// A <see cref="Result{T}"/> containing <c>true</c> if validation succeeds,
+    /// or <c>Failure</c> with the first error message if validation fails.
+    /// </returns>
+    protected static async Task<Result<bool>> ValidateAsync<TCommand>(IValidator<TCommand>? validator, TCommand command)
+    {
+        if (validator is null)
+        {
+            return await Result<bool>.SuccessAsync(true);
+        }
+
+        var result = await validator.ValidateAsync(command);
+        if (!result.IsValid)
+        {
+            return await Result<bool>.FailureAsync(
+                TinyResult.Enums.ErrorCode.ValidationError,
+                result.Errors[0].ErrorMessage);
+        }
+
+        return await Result<bool>.SuccessAsync(true);
+    }
 }
