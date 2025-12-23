6f82841a5d49952fd2778bcec094900b44391ee7
Serhii Moskalets <s.mosklaets16@gmail.com>
2025-12-23 16:12:12 +0100

    test(domain): add tests for TagEntity
    - Added tests for constructor with valid and invalid inputs
    - Added tests to ensure trimming of whitespace in constructor
    - Added test to verify initialization of Tasks collection

---
+++ Backend/tests/TodoListApp.Domain.Test/Entities/TagEntityTests.cs
@@ -0,0 +1,47 @@
+﻿using TodoListApp.Domain.Entities;
+
+namespace TodoListApp.Domain.Test.Entities;
+
+public class TagEntityTests
+{
+    [Fact]
+    public void Constructor_ShouldCreateTag_WhenValidData()
+    {
+        var name = "Tag";
+        var userId = Guid.NewGuid();
+
+        var tag = new TagEntity(name, userId);
+        Assert.Equal(name, tag.Name);
+        Assert.Equal(userId, tag.UserId);
+    }
+
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    [InlineData("   ")]
+    public void Constructor_ShouldThrow_ThenTextInvalid(string? invalidText)
+    {
+        var userId = Guid.NewGuid();
+
+        Assert.Throws<ArgumentException>(() => new TagEntity(invalidText!, userId));
+    }
+
+    [Theory]
+    [InlineData("   Tag   ")]
+    [InlineData("Tag   ")]
+    [InlineData("   Tag")]
+    public void Constructor_ShouldTrimName(string name)
+    {
+        var userId = Guid.NewGuid();
+        var tag = new TagEntity(name, userId);
+        Assert.Equal("Tag", tag.Name);
+    }
+
+    [Fact]
+    public void Constructor_ShouldInitializeTasksCollection()
+    {
+        var tag = new TagEntity("Tag", Guid.NewGuid());
+        Assert.NotNull(tag.Tasks);
+        Assert.Empty(tag.Tasks);
+    }
+}
