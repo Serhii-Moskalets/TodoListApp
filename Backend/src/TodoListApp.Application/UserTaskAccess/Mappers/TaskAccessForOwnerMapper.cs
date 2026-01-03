70b987b9c52246c5fa1588db4cd5708e9bf898b8
Serhii Moskalets <s.mosklaets16@gmail.com>
2026-01-03 22:58:41 +0100

    refactor(mapper): simplify task access mapper responsibilities

--- Backend/src/TodoListApp.Application/UserTaskAccess/Mappers/TaskAccessForOwnerMapper.cs
+++ Backend/src/TodoListApp.Application/UserTaskAccess/Mappers/TaskAccessForOwnerMapper.cs
@@ -1,6 +1,5 @@
 ﻿using Riok.Mapperly.Abstractions;
 using TodoListApp.Application.Common.Dtos;
-using TodoListApp.Application.UserTaskAccess.Dtos;
 using TodoListApp.Domain.Entities;
 
 namespace TodoListApp.Application.UserTaskAccess.Mappers;
@@ -28,19 +27,5 @@ public static partial class TaskAccessForOwnerMapper
     /// </summary>
     /// <param name="entities">The collection of user-task access entities.</param>
     /// <returns>A list of <see cref="UserBriefDto"/> objects.</returns>
-    public static partial IList<UserBriefDto> Map(IReadOnlyCollection<UserTaskAccessEntity> entities);
-
-    /// <summary>
-    /// Maps a collection of <see cref="UserTaskAccessEntity"/> to a <see cref="TaskAccessListDto"/>,
-    /// including the task details and the list of users who have access.
-    /// </summary>
-    /// <param name="entities">The collection of user-task access entities for a task.</param>
-    /// <returns>A <see cref="TaskAccessListDto"/> containing task information and associated users.</returns>
-    public static TaskAccessListDto MapToTaskAccess(IReadOnlyCollection<UserTaskAccessEntity> entities)
-        => new ()
-        {
-            Id = entities.FirstOrDefault()?.TaskId ?? Guid.Empty,
-            Title = entities.FirstOrDefault()?.Task.Title ?? string.Empty,
-            Users = Map(entities) ?? [],
-        };
+    public static partial IReadOnlyCollection<UserBriefDto> Map(IReadOnlyCollection<UserTaskAccessEntity> entities);
 }
