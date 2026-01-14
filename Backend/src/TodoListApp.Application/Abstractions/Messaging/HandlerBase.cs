aed6f7c97da8664b701db835204b451b2ea926b5
Serhii Moskalets <s.mosklaets16@gmail.com>
2026-01-14 20:38:58 +0100

    refactor: migrate to MediatR infrastructure and global validation
    - Added ICommand and IQuery abstractions based on MediatR
    - Implemented ValidationBehavior for automatic FluentValidation
    - Configured MediatR assembly scanning in ServiceCollectionExtensions
    - Removed ValidateAsync method from HandlerBase as validation is now handled
      globally by ValidationBehavior in the MediatR pipeline.
    - Removed legacy custom CQRS interfaces

--- Backend/src/TodoListApp.Application/Abstractions/Messaging/HandlerBase.cs
+++ Backend/src/TodoListApp.Application/Abstractions/Messaging/HandlerBase.cs
@@ -1,53 +1,19 @@
-﻿using FluentValidation;
-using TinyResult;
-using TodoListApp.Application.Abstractions.Interfaces.UnitOfWork;
+﻿using TodoListApp.Application.Abstractions.Interfaces.UnitOfWork;
 
 namespace TodoListApp.Application.Abstractions.Messaging;
 
 /// <summary>
 /// Abstract base class for all handlers (commands and queries) in the CQRS pattern.
-/// Provides shared logic and services that all handlers may need,
-/// such as access to <see cref="IUnitOfWork"/> for repository operations.
+/// Provides shared access to the <see cref="IUnitOfWork"/> for repository operations.
 /// </summary>
 /// <remarks>
-/// Initializes a new instance of the <see cref="HandlerBase"/> class with the specified unit of work.
+/// Initializes a new instance of the <see cref="HandlerBase"/> class.
 /// </remarks>
-/// <param name="unitOfWork">An <see cref="IUnitOfWork"/> instance for working with repositories.</param>
+/// <param name="unitOfWork">The unit of work instance for accessing repositories.</param>
 public abstract class HandlerBase(IUnitOfWork unitOfWork)
 {
     /// <summary>
     /// Gets the unit of work for accessing repositories.
     /// </summary>
     protected IUnitOfWork UnitOfWork { get; } = unitOfWork;
-
-    /// <summary>
-    /// Performs validation of the given command or query using FluentValidation.
-    /// </summary>
-    /// <typeparam name="TCommand">The type of the command or query to validate.</typeparam>
-    /// <param name="validator">
-    /// The validator instance for the specific command or query type.
-    /// If <c>null</c> is provided, validation is skipped.
-    /// </param>
-    /// <param name="command">The command or query instance to validate.</param>
-    /// <returns>
-    /// A <see cref="Result{T}"/> containing <c>true</c> if validation succeeds,
-    /// or <c>Failure</c> with the first error message if validation fails.
-    /// </returns>
-    protected static async Task<Result<bool>> ValidateAsync<TCommand>(IValidator<TCommand>? validator, TCommand command)
-    {
-        if (validator is null)
-        {
-            return await Result<bool>.SuccessAsync(true);
-        }
-
-        var result = await validator.ValidateAsync(command);
-        if (!result.IsValid)
-        {
-            return await Result<bool>.FailureAsync(
-                TinyResult.Enums.ErrorCode.ValidationError,
-                result.Errors[0].ErrorMessage);
-        }
-
-        return await Result<bool>.SuccessAsync(true);
-    }
 }
--- Backend/src/TodoListApp.Application/Extensions/ServiceCollectionExtensions.cs
+++ Backend/src/TodoListApp.Application/Extensions/ServiceCollectionExtensions.cs
@@ -1,153 +1,35 @@
 ﻿using FluentValidation;
 using Microsoft.Extensions.DependencyInjection;
+using TodoListApp.Application.Abstractions.Behaviors;
 using TodoListApp.Application.Abstractions.Interfaces.Services;
-using TodoListApp.Application.Abstractions.Messaging;
-using TodoListApp.Application.Comment.Commands.CreateComment;
-using TodoListApp.Application.Comment.Commands.DeleteComment;
-using TodoListApp.Application.Comment.Commands.UpdateComment;
-using TodoListApp.Application.Comment.Queries.GetComments;
-using TodoListApp.Application.Common.Dtos;
 using TodoListApp.Application.Common.Services;
-using TodoListApp.Application.Tag.Commands.CreateTag;
-using TodoListApp.Application.Tag.Commands.DeleteTag;
-using TodoListApp.Application.Tag.Queries.GetAllTags;
-using TodoListApp.Application.TaskList.Commands.CreateTaskList;
-using TodoListApp.Application.TaskList.Commands.DeleteTaskList;
-using TodoListApp.Application.TaskList.Commands.UpdateTaskList;
-using TodoListApp.Application.TaskList.Dtos;
-using TodoListApp.Application.TaskList.Queries.GetAllTaskList;
-using TodoListApp.Application.Tasks.Commands.AddTagToTask;
-using TodoListApp.Application.Tasks.Commands.ChangeTaskStatus;
-using TodoListApp.Application.Tasks.Commands.CreateTask;
-using TodoListApp.Application.Tasks.Commands.DeleteOverdueTasks;
-using TodoListApp.Application.Tasks.Commands.DeleteTask;
-using TodoListApp.Application.Tasks.Commands.RemoveTagFromTask;
-using TodoListApp.Application.Tasks.Commands.UpdateTask;
-using TodoListApp.Application.Tasks.Queries.GetTaskById;
-using TodoListApp.Application.Tasks.Queries.GetTaskByTitle;
-using TodoListApp.Application.Tasks.Queries.GetTasks;
-using TodoListApp.Application.UserTaskAccess.Commands.CreateUserTaskAccess;
-using TodoListApp.Application.UserTaskAccess.Commands.DeleteTaskAccessById;
-using TodoListApp.Application.UserTaskAccess.Commands.DeleteTaskAccessByUserEmail;
-using TodoListApp.Application.UserTaskAccess.Commands.DeleteTaskAccessesByTask;
-using TodoListApp.Application.UserTaskAccess.Commands.DeleteTaskAccessesByUser;
-using TodoListApp.Application.UserTaskAccess.Dtos;
-using TodoListApp.Application.UserTaskAccess.Queries.GetSharedTaskById;
-using TodoListApp.Application.UserTaskAccess.Queries.GetSharedTasksByUserId;
-using TodoListApp.Application.UserTaskAccess.Queries.GetTaskWithSharedUsers;
 
 namespace TodoListApp.Application.Extensions;
 
 /// <summary>
-/// Provides extension methods to register all application-level services,
-/// including FluentValidation validators and CQRS command/query handlers.
+/// Provides extension methods for registering application layer services in the dependency injection container.
 /// </summary>
 public static class ServiceCollectionExtensions
 {
     /// <summary>
-    /// Registers all application services into the dependency injection container.
+    /// Registers MediatR, FluentValidation, and custom application services.
     /// </summary>
-    /// <param name="services">The service collection to register services into.</param>
+    /// <param name="services">The service collection to add services to.</param>
     public static void AddApplicationServices(this IServiceCollection services)
     {
-        AddApplicationValidators(services);
-        AddApplicationHandlers(services);
+        var assembly = typeof(ServiceCollectionExtensions).Assembly;
 
-        // --- Application Services ---
+        services.AddMediatR(config =>
+        {
+            config.RegisterServicesFromAssembly(assembly);
+            config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+        });
+
+        services.AddValidatorsFromAssembly(assembly);
+
+        // --- Custom Application Services ---
         services.AddScoped<ITaskAccessService, TaskAccessService>();
         services.AddScoped<ITaskListNameUniquenessService, TaskListNameUniquenessService>();
         services.AddScoped<IUserTaskAccessService, UserTaskAccessService>();
-    }
-
-    /// <summary>
-    /// Registers all FluentValidation validators used in the application.
-    /// </summary>
-    /// <param name="services">The service collection.</param>
-    private static void AddApplicationValidators(this IServiceCollection services)
-    {
-        // --- Tag Validators ---
-        services.AddScoped<IValidator<CreateTagCommand>, CreateTagCommandValidator>();
-        services.AddScoped<IValidator<DeleteTagCommand>, DeleteTagCommandValidator>();
-        services.AddScoped<IValidator<GetAllTagsQuery>, GetAllTagsQueryValidator>();
-
-        // --- Comment Validators ---
-        services.AddScoped<IValidator<CreateCommentCommand>, CreateCommentCommandValidator>();
-        services.AddScoped<IValidator<DeleteCommentCommand>, DeleteCommentCommandValidator>();
-        services.AddScoped<IValidator<UpdateCommentCommand>, UpdateCommentCommandValidator>();
-        services.AddScoped<IValidator<GetCommentsQuery>, GetCommentsQueryValidator>();
-
-        // --- Task Validators ---
-        services.AddScoped<IValidator<AddTagToTaskCommand>, AddTagToTaskCommandValidator>();
-        services.AddScoped<IValidator<ChangeTaskStatusCommand>, ChangeTaskStatusCommandValidator>();
-        services.AddScoped<IValidator<CreateTaskCommand>, CreateTaskCommandValidator>();
-        services.AddScoped<IValidator<DeleteOverdueTasksCommand>, DeleteOverdueTasksCommandValidator>();
-        services.AddScoped<IValidator<DeleteTaskCommand>, DeleteTaskCommandValidator>();
-        services.AddScoped<IValidator<RemoveTagFromTaskCommand>, RemoveTagFromTaskCommandValidator>();
-        services.AddScoped<IValidator<UpdateTaskCommand>, UpdateTaskCommandValidator>();
-        services.AddScoped<IValidator<GetTaskByIdQuery>, GetTaskByIdQueryValidator>();
-        services.AddScoped<IValidator<GetTaskByTitleQuery>, GetTaskByTitleQueryValidator>();
-        services.AddScoped<IValidator<GetTasksQuery>, GetTasksQueryValidator>();
-
-        // --- TaskList Validators ---
-        services.AddScoped<IValidator<CreateTaskListCommand>, CreateTaskListCommandValidator>();
-        services.AddScoped<IValidator<DeleteTaskListCommand>, DeleteTaskListCommandValidator>();
-        services.AddScoped<IValidator<UpdateTaskListCommand>, UpdateTaskListCommandValidator>();
-        services.AddScoped<IValidator<GetAllTaskListQuery>, GetAllTaskListQueryValidator>();
-
-        // --- UserTaskAccess Validators ---
-        services.AddScoped<IValidator<CreateUserTaskAccessCommand>, CreateUserTaskAccessCommandValidator>();
-        services.AddScoped<IValidator<DeleteTaskAccessByIdCommand>, DeleteTaskAccessByIdCommandValidator>();
-        services.AddScoped<IValidator<DeleteTaskAccessByUserEmailCommand>, DeleteTaskAccessByUserEmailCommandValidator>();
-        services.AddScoped<IValidator<DeleteTaskAccessesByTaskCommand>, DeleteTaskAccessesByTaskCommandValidator>();
-        services.AddScoped<IValidator<DeleteTaskAccessesByUserCommand>, DeleteTaskAccessesByUserCommandValidator>();
-        services.AddScoped<IValidator<GetSharedTaskByIdQuery>, GetSharedTaskByIdQueryValidator>();
-        services.AddScoped<IValidator<GetSharedTasksByUserIdQuery>, GetSharedTasksByUserIdQueryValidator>();
-        services.AddScoped<IValidator<GetTaskWithSharedUsersQuery>, GetTaskWithSharedUsersQueryValidator>();
-    }
-
-    /// <summary>
-    /// Registers all command and query handlers used in the application.
-    /// </summary>
-    /// <param name="services">The service collection.</param>
-    private static void AddApplicationHandlers(this IServiceCollection services)
-    {
-        // --- Tag Handlers ---
-        services.AddScoped<ICommandHandler<CreateTagCommand, Guid>, CreateTagCommandHandler>();
-        services.AddScoped<ICommandHandler<DeleteTagCommand, bool>, DeleteTagCommandHandler>();
-        services.AddScoped<IQueryHandler<GetAllTagsQuery, IEnumerable<TagDto>>, GetAllTagsQueryHandler>();
-
-        // --- Comment Handlers ---
-        services.AddScoped<ICommandHandler<CreateCommentCommand, Guid>, CreateCommentCommandHandler>();
-        services.AddScoped<ICommandHandler<DeleteCommentCommand, bool>, DeleteCommentCommandHandler>();
-        services.AddScoped<ICommandHandler<UpdateCommentCommand, bool>, UpdateCommentCommandHandler>();
-        services.AddScoped<IQueryHandler<GetCommentsQuery, IEnumerable<CommentDto>>, GetCommentsQueryHandler>();
-
-        // --- Task Handlers ---
-        services.AddScoped<ICommandHandler<AddTagToTaskCommand, bool>, AddTagToTaskCommandHandler>();
-        services.AddScoped<ICommandHandler<ChangeTaskStatusCommand, bool>, ChangeTaskStatusCommandHandler>();
-        services.AddScoped<ICommandHandler<CreateTaskCommand, Guid>, CreateTaskCommandHandler>();
-        services.AddScoped<ICommandHandler<DeleteOverdueTasksCommand, bool>, DeleteOverdueTasksCommandHandler>();
-        services.AddScoped<ICommandHandler<DeleteTaskCommand, bool>, DeleteTaskCommandHandler>();
-        services.AddScoped<ICommandHandler<RemoveTagFromTaskCommand, bool>, RemoveTagFromTaskCommandHandler>();
-        services.AddScoped<ICommandHandler<UpdateTaskCommand, bool>, UpdateTaskCommandHandler>();
-        services.AddScoped<IQueryHandler<GetTaskByIdQuery, TaskDto>, GetTaskByIdQueryHandler>();
-        services.AddScoped<IQueryHandler<GetTaskByTitleQuery, IEnumerable<TaskDto>>, GetTaskByTitleQueryHandler>();
-        services.AddScoped<IQueryHandler<GetTasksQuery, IEnumerable<TaskDto>>, GetTasksQueryHandler>();
-
-        // --- TaskList Handlers ---
-        services.AddScoped<ICommandHandler<CreateTaskListCommand, Guid>, CreateTaskListCommandHandler>();
-        services.AddScoped<ICommandHandler<DeleteTaskListCommand, bool>, DeleteTaskListCommandHandler>();
-        services.AddScoped<ICommandHandler<UpdateTaskListCommand, bool>, UpdateTaskListCommandHandler>();
-        services.AddScoped<IQueryHandler<GetAllTaskListQuery, IEnumerable<TaskListDto>>, GetAllTaskListQueryHandler>();
-
-        // --- UserTaskAccess Handlers ---
-        services.AddScoped<ICommandHandler<CreateUserTaskAccessCommand, bool>, CreateUserTaskAccessCommandHandler>();
-        services.AddScoped<ICommandHandler<DeleteTaskAccessesByTaskCommand, bool>, DeleteTaskAccessesByTaskCommandHandler>();
-        services.AddScoped<ICommandHandler<DeleteTaskAccessesByUserCommand, bool>, DeleteTaskAccessesByUserCommandHandler>();
-        services.AddScoped<ICommandHandler<DeleteTaskAccessByUserEmailCommand, bool>, DeleteTaskAccessByUserEmailCommandHandler>();
-        services.AddScoped<ICommandHandler<DeleteTaskAccessByIdCommand, bool>, DeleteTaskAccessByIdCommandHandler>();
-        services.AddScoped<IQueryHandler<GetSharedTaskByIdQuery, TaskDto>, GetSharedTaskByIdQueryHandler>();
-        services.AddScoped<IQueryHandler<GetTaskWithSharedUsersQuery, TaskAccessListDto>, GetTaskWithSharedUsersQueryHandler>();
-        services.AddScoped<IQueryHandler<GetSharedTasksByUserIdQuery, IEnumerable<TaskDto>>, GetSharedTasksByUserIdQueryHandler>();
     }
 }
