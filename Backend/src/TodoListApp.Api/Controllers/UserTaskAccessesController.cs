b30d90626e8335d95b7c7425e532fdf26d20d8ad
Serhii Moskalets <s.mosklaets16@gmail.com>
2026-01-23 22:03:53 +0100

    refactor(api): migrate UserTaskAccessesController to MediatR and implement pagination
    - Removed 8 specific ICommandHandler and IQueryHandler injections and deleted the constructor.
    - Replaced manual handler calls with 'this.Mediator.Send' inherited from BaseController.
    - Introduced pagination (page/pageSize) for 'GetSharedTasksByUserId' and 'GetUsersWithTaskAccess' queries.
    - Cleaned up action method names by removing the unnecessary 'Async' suffix.
    - Simplified 'CreateUserTaskAccess' by delegating response handling to the base 'HandleResult' method.
    - Standardized namespaces and removed unused 'Application.Abstractions.Messaging' and DTO imports.

--- Backend/src/TodoListApp.Api/Controllers/UserTaskAccessesController.cs
+++ Backend/src/TodoListApp.Api/Controllers/UserTaskAccessesController.cs
@@ -1,19 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using TodoListApp.Api.DTOs.UserTaskAccess;
-using TodoListApp.Application.Abstractions.Messaging;
-using TodoListApp.Application.Common.Dtos;
+using TodoListApp.Api.Requests.UserTaskAccess;
 using TodoListApp.Application.UserTaskAccess.Commands.CreateUserTaskAccess;
 using TodoListApp.Application.UserTaskAccess.Commands.DeleteTaskAccessById;
 using TodoListApp.Application.UserTaskAccess.Commands.DeleteTaskAccessByUserEmail;
 using TodoListApp.Application.UserTaskAccess.Commands.DeleteTaskAccessesByTask;
 using TodoListApp.Application.UserTaskAccess.Commands.DeleteTaskAccessesByUser;
-using TodoListApp.Application.UserTaskAccess.Dtos;
 using TodoListApp.Application.UserTaskAccess.Queries.GetSharedTaskById;
 using TodoListApp.Application.UserTaskAccess.Queries.GetSharedTasksByUserId;
-using TodoListApp.Application.UserTaskAccess.Queries.GetTaskWithSharedUsers;
+using TodoListApp.Application.UserTaskAccess.Queries.GetUsersWithTaskAccess;
 
 namespace TodoListApp.Api.Controllers;
 
@@ -22,72 +18,32 @@ namespace TodoListApp.Api.Controllers;
 /// Provides endpoints to retrieve shared tasks, get users with access to a task,
 /// and manage access assignments and deletions.
 /// </summary>
-[ApiController]
-[Route("api/[controller]")]
 public class UserTaskAccessesController : BaseController
 {
-    private readonly IQueryHandler<GetSharedTaskByIdQuery, TaskDto> _getSharedTaskByIdHandler;
-    private readonly IQueryHandler<GetSharedTasksByUserIdQuery, IEnumerable<TaskDto>> _getSharedTasksByUserIdHandler;
-    private readonly IQueryHandler<GetTaskWithSharedUsersQuery, TaskAccessListDto> _getTaskWithSharedUsersHandler;
-    private readonly ICommandHandler<CreateUserTaskAccessCommand, bool> _createUserTaskAccessHandler;
-    private readonly ICommandHandler<DeleteTaskAccessesByTaskCommand, bool> _deleteTaskAccessesByTaskHandler;
-    private readonly ICommandHandler<DeleteTaskAccessesByUserCommand, bool> _deleteTaskAccessesByUserHandler;
-    private readonly ICommandHandler<DeleteTaskAccessByUserEmailCommand, bool> _deleteTaskAccessByUserEmailHandler;
-    private readonly ICommandHandler<DeleteTaskAccessByIdCommand, bool> _deleteTaskAccessByIdHandler;
-
-    /// <summary>
-    /// Initializes a new instance of the <see cref="UserTaskAccessesController"/> class.
-    /// </summary>
-    /// <param name="getSharedTaskByIdHandler">Handler for retrieving a shared task by its ID.</param>
-    /// <param name="getSharedTasksByUserIdHandler">Handler for retrieving all tasks shared with a user.</param>
-    /// <param name="getTaskWithSharedUsersHandler">Handler for retrieving task details along with users who have access.</param>
-    /// <param name="createUserTaskAccessHandler">Handler for creating a new user-task access.</param>
-    /// <param name="deletTaskAccessesByTaskHadler">Handler for deleting all task accesses by task ID.</param>
-    /// <param name="deleteTaskAccessesByUserHandler">Handler for deleting all task accesses by user ID.</param>
-    /// <param name="deleteTaskAccessByUserEmailHandler">Handler for deleting a task access by user email.</param>
-    /// <param name="deleteTaskAccessByIdHandler">Handler for deleting a task access by its ID.</param>
-    public UserTaskAccessesController(
-        IQueryHandler<GetSharedTaskByIdQuery, TaskDto> getSharedTaskByIdHandler,
-        IQueryHandler<GetSharedTasksByUserIdQuery, IEnumerable<TaskDto>> getSharedTasksByUserIdHandler,
-        IQueryHandler<GetTaskWithSharedUsersQuery, TaskAccessListDto> getTaskWithSharedUsersHandler,
-        ICommandHandler<CreateUserTaskAccessCommand, bool> createUserTaskAccessHandler,
-        ICommandHandler<DeleteTaskAccessesByTaskCommand, bool> deletTaskAccessesByTaskHadler,
-        ICommandHandler<DeleteTaskAccessesByUserCommand, bool> deleteTaskAccessesByUserHandler,
-        ICommandHandler<DeleteTaskAccessByUserEmailCommand, bool> deleteTaskAccessByUserEmailHandler,
-        ICommandHandler<DeleteTaskAccessByIdCommand, bool> deleteTaskAccessByIdHandler)
-    {
-        this._getSharedTaskByIdHandler = getSharedTaskByIdHandler;
-        this._getSharedTasksByUserIdHandler = getSharedTasksByUserIdHandler;
-        this._getTaskWithSharedUsersHandler = getTaskWithSharedUsersHandler;
-        this._createUserTaskAccessHandler = createUserTaskAccessHandler;
-        this._deleteTaskAccessesByTaskHandler = deletTaskAccessesByTaskHadler;
-        this._deleteTaskAccessesByUserHandler = deleteTaskAccessesByUserHandler;
-        this._deleteTaskAccessByUserEmailHandler = deleteTaskAccessByUserEmailHandler;
-        this._deleteTaskAccessByIdHandler = deleteTaskAccessByIdHandler;
-    }
-
     /// <summary>
     /// Retrieves a shared task by its ID for the current user.
     /// </summary>
     /// <param name="taskId">The unique identifier of the task.</param>
     /// <returns>An <see cref="IActionResult"/> containing the task details if found; otherwise, an error response.</returns>
     [HttpGet("tasks/{taskId:guid}/shared")]
-    public async Task<IActionResult> GetSharedTaskByIdAsync([FromRoute] Guid taskId)
+    public async Task<IActionResult> GetSharedTaskById([FromRoute] Guid taskId)
     {
         var query = new GetSharedTaskByIdQuery(taskId, CurrentUserId);
-        var result = await this._getSharedTaskByIdHandler.Handle(query, this.HttpContext.RequestAborted);
+        var result = await this.Mediator.Send(query, this.HttpContext.RequestAborted);
         return this.HandleResult(result);
     }
 
     /// <summary>
     /// Retrieves all tasks that are shared with the current user.
     /// </summary>
+    /// <param name="page">The page number (1-based).</param>
+    /// <param name="pageSize">The number of items per page.</param>
     /// <returns>An <see cref="IActionResult"/> containing a list of tasks shared with the user.</returns>
     [HttpGet]
-    public async Task<IActionResult> GetSharedTasksByUserIdAsync()
+    public async Task<IActionResult> GetSharedTasksByUserId([FromQuery] int page = 1, [FromQuery] int pageSize = 10)
     {
-        var query = new GetSharedTasksByUserIdQuery(CurrentUserId);
-        var result = await this._getSharedTasksByUserIdHandler.Handle(query, this.HttpContext.RequestAborted);
+        var query = new GetSharedTasksByUserIdQuery(CurrentUserId, page, pageSize);
+        var result = await this.Mediator.Send(query, this.HttpContext.RequestAborted);
         return this.HandleResult(result);
     }
 
@@ -95,12 +51,14 @@ public class UserTaskAccessesController : BaseController
     /// Retrieves a task along with all users who have access to it.
     /// </summary>
     /// <param name="taskId">The unique identifier of the task.</param>
+    /// <param name="page">The page number (1-based).</param>
+    /// <param name="pageSize">The number of items per page.</param>
     /// <returns>An <see cref="IActionResult"/> containing the task access list details.</returns>
     [HttpGet("tasks/{taskId:guid}/users")]
-    public async Task<IActionResult> GetTaskWithSharedUsersAsync([FromRoute] Guid taskId)
+    public async Task<IActionResult> GetUsersWithTaskAccess([FromRoute] Guid taskId, [FromQuery] int page = 1, [FromQuery] int pageSize = 10)
     {
-        var query = new GetTaskWithSharedUsersQuery(taskId, CurrentUserId);
-        var result = await this._getTaskWithSharedUsersHandler.Handle(query, this.HttpContext.RequestAborted);
+        var query = new GetUsersWithTaskAccessQuery(taskId, CurrentUserId, page, pageSize);
+        var result = await this.Mediator.Send(query, this.HttpContext.RequestAborted);
         return this.HandleResult(result);
     }
 
@@ -111,16 +69,11 @@ public class UserTaskAccessesController : BaseController
     /// <param name="request">Request containing the email of the user to share the task with.</param>
     /// <returns>An <see cref="IActionResult"/> indicating success or failure of the operation.</returns>
     [HttpPost("tasks/{taskId:guid}/share-task")]
-    public async Task<IActionResult> CreateUserTaskAccessAsync([FromRoute] Guid taskId, [FromBody] AccessEmailRequest request)
+    public async Task<IActionResult> CreateUserTaskAccess([FromRoute] Guid taskId, [FromBody] AccessEmailRequest request)
     {
         var command = new CreateUserTaskAccessCommand(taskId, CurrentUserId, request.Email);
-        var result = await this._createUserTaskAccessHandler.HandleAsync(command, this.HttpContext.RequestAborted);
-        if (!result.IsSuccess)
-        {
-            return this.HandleResult(result);
-        }
-
-        return this.Ok(new { TaskId = taskId, request.Email });
+        var result = await this.Mediator.Send(command, this.HttpContext.RequestAborted);
+        return this.HandleResult(result);
     }
 
     /// <summary>
@@ -130,10 +83,10 @@ public class UserTaskAccessesController : BaseController
     /// <param name="email">The email of the user whose access should be removed.</param>
     /// <returns>An <see cref="IActionResult"/> indicating the outcome of the deletion.</returns>
     [HttpDelete("tasks/{taskId:guid}/by-email")]
-    public async Task<IActionResult> DeleteTaskAccessByEmailAsync([FromRoute] Guid taskId, [FromQuery] string? email)
+    public async Task<IActionResult> DeleteTaskAccessByEmail([FromRoute] Guid taskId, [FromQuery] string? email)
     {
         var command = new DeleteTaskAccessByUserEmailCommand(taskId, CurrentUserId, email);
-        var result = await this._deleteTaskAccessByUserEmailHandler.HandleAsync(command, this.HttpContext.RequestAborted);
+        var result = await this.Mediator.Send(command, this.HttpContext.RequestAborted);
         return this.HandleNoContent(result);
     }
 
@@ -144,10 +97,10 @@ public class UserTaskAccessesController : BaseController
     /// <param name="userId">The unique identifier of the user whose access will be removed.</param>
     /// <returns>An <see cref="IActionResult"/> indicating the outcome of the deletion.</returns>
     [HttpDelete("tasks/{taskId:guid}/users/{userId:guid}")]
-    public async Task<IActionResult> DeleteTaskAccessByIdAsync([FromRoute] Guid taskId, [FromRoute] Guid userId)
+    public async Task<IActionResult> DeleteTaskAccessById([FromRoute] Guid taskId, [FromRoute] Guid userId)
     {
         var command = new DeleteTaskAccessByIdCommand(taskId, userId);
-        var result = await this._deleteTaskAccessByIdHandler.HandleAsync(command, this.HttpContext.RequestAborted);
+        var result = await this.Mediator.Send(command, this.HttpContext.RequestAborted);
         return this.HandleNoContent(result);
     }
 
@@ -157,10 +110,10 @@ public class UserTaskAccessesController : BaseController
     /// <param name="taskId">The unique identifier of the task.</param>
     /// <returns>An <see cref="IActionResult"/> indicating the outcome of the deletion.</returns>
     [HttpDelete("tasks/{taskId:guid}")]
-    public async Task<IActionResult> DeleteTaskAccessesByTaskAsync([FromRoute] Guid taskId)
+    public async Task<IActionResult> DeleteTaskAccessesByTask([FromRoute] Guid taskId)
     {
         var command = new DeleteTaskAccessesByTaskCommand(taskId, CurrentUserId);
-        var result = await this._deleteTaskAccessesByTaskHandler.HandleAsync(command, this.HttpContext.RequestAborted);
+        var result = await this.Mediator.Send(command, this.HttpContext.RequestAborted);
         return this.HandleNoContent(result);
     }
 
@@ -169,10 +122,10 @@ public class UserTaskAccessesController : BaseController
     /// </summary>
     /// <returns>An <see cref="IActionResult"/> indicating the outcome of the deletion.</returns>
     [HttpDelete("users/my")]
-    public async Task<IActionResult> DeleteTasksAccessesByUserAsync()
+    public async Task<IActionResult> DeleteTasksAccessesByUser()
     {
         var command = new DeleteTaskAccessesByUserCommand(CurrentUserId);
-        var result = await this._deleteTaskAccessesByUserHandler.HandleAsync(command, this.HttpContext.RequestAborted);
+        var result = await this.Mediator.Send(command, this.HttpContext.RequestAborted);
         return this.HandleNoContent(result);
     }
 }
